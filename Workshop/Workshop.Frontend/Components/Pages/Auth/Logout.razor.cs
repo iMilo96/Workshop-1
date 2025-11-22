2c63325be85d1abcca2e85d8c47e091f07b77c83
Camilo Gutiérrez <cmilorollins@gmail.com>
2025-11-21 20:16:14 -0500

    Basic register user

--- Workshop/Workshop.Frontend/Components/Pages/Auth/Logout.razor.cs
+++ Workshop/Workshop.Frontend/Components/Pages/Auth/Logout.razor.cs
@@ -1,5 +1,23 @@
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Workshop.Frontend.Services;
+
 namespace Workshop.Frontend.Components.Pages.Auth;
 
 public partial class Logout
 {
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ILoginService LoginService { get; set; } = null!;
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    private async Task LogoutActionAsync()
+    {
+        await LoginService.LogoutAsync();
+        CancelAction();
+    }
+
+    private void CancelAction()
+    {
+        MudDialog.Cancel();
+    }
 }
